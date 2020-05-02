cf61a9c58865fb18f98cf3b3e6a09e2d22f60c75
Zaur Guliyev <zaur@zaurs-mbp.attlocal.net>
2020-05-02 13:48:04 -0500

    mvp complete

--- adv.py
+++ adv.py
@@ -25,12 +25,56 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+##########################################
+# My code starts below
 
 
+def traverse(graph):
+    # placeholder for all
+    maze = []
+    # placeholder for rooms
+    map_ = []
+    # set of visited rooms
+    visited = set()
+    # initialize maze with 0
+    maze.append(0)
+
+    while len(visited) < len(graph):
+        # Holder for current room
+        current = maze[-1]
+        # Add to visited
+        visited.add(current)
+        # Grab all possible exits for current room
+        conns = graph[current][-1]
+        unvisited_conns = []
+        # If any connected rooms have not been visited yet, we add it to list if unvisited rooms
+        for k, v in conns.items():
+            if v not in visited:
+                unvisited_conns.append(v)
+        # This is basically checking for a dead end. If there are rooms connected that we haven't visited, we visit them.
+        # If there aren't, we go back to last room
+        if len(unvisited_conns) > 0:
+            room = unvisited_conns[0]
+            maze.append(room)
+        else:
+            room = maze[-2]
+            maze.pop()
+        # Loop through the curr rooms exits. Find the exit that equals the next room to be visited and pass that key into the map_
+        for last_room, exits in conns.items():
+            if exits == room:
+                map_.append(last_room)
+    # print(rooms)
+    return map_
+# 997 moves, 500 rooms visited 
+
+traversal_path = traverse(room_graph)
+
+# End
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -51,12 +95,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
